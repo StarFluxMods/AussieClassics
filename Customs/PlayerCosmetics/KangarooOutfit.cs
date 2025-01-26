cac2235bb05e6250fdcc4616feec38a495b02e78
Lachlan Leone <starfluxgames@gmail.com>
2025-01-26 16:18:11 +1100

    Adjust Hat

--- Customs/PlayerCosmetics/KangarooOutfit.cs
+++ Customs/PlayerCosmetics/KangarooOutfit.cs
@@ -11,14 +11,13 @@ namespace AussieClassics.Customs.PlayerCosmetics
         public override string UniqueNameID => "KangarooOutfit";
         public override CosmeticType CosmeticType => CosmeticType.Outfit;
         public override GameObject Visual => Mod.Bundle.LoadAsset<GameObject>("KangarooHead").AssignMaterialsByNames();
-        // public override bool BlockHats => true;
         public override float HeadSize => 0.4f;
 
         public override void OnRegister(PlayerCosmetic gameDataObject)
         {
             PlayerOutfitComponent playerOutfitComponent = gameDataObject.Visual.AddComponent<PlayerOutfitComponent>();
             playerOutfitComponent.Renderers.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Head").GetComponent<SkinnedMeshRenderer>());
-            //playerOutfitComponent.Hats.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Head").GetComponent<SkinnedMeshRenderer>());
+            playerOutfitComponent.Hats.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Head").GetComponent<SkinnedMeshRenderer>());
             playerOutfitComponent.Renderers.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Tail").GetComponent<SkinnedMeshRenderer>());
             
         }
