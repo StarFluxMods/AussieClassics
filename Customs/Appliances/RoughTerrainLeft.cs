ebe36ac416e5dbe7faa9038ae53b7662772d80a8
Lachlan Leone <lachlan@starfluxgames.com>
2025-01-27 00:58:11 +1100

    Fixed Nullref

--- Customs/Appliances/RoughTerrainLeft.cs
+++ Customs/Appliances/RoughTerrainLeft.cs
@@ -10,21 +10,5 @@ namespace AussieClassics.Customs.Appliances
     {
         public override string UniqueNameID => "RoughTerrainLeft";
         public override GameObject Prefab => Mod.Bundle.LoadAsset<GameObject>("RoughTerrainLeft").AssignMaterialsByNames();
-        
-        public override void OnRegister(Appliance gameDataObject)
-        {
-            base.OnRegister(gameDataObject);
-            
-            GameObject Car = gameDataObject.Prefab.GetChild("Cars/CarContainer1/Car");
-            CarView view = Car.AddComponent<CarView>();
-            view.Car = Car;
-            view.animator = Car.GetComponentInChildren<Animator>();
-            
-            
-            GameObject Car2 = gameDataObject.Prefab.GetChild("Cars/CarContainer2/Car");
-            CarView view2 = Car2.AddComponent<CarView>();
-            view2.Car = Car2;
-            view2.animator = Car2.GetComponentInChildren<Animator>();
-        }
     }
 }
