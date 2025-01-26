bdbc3ac05314e580fb5138bbe088e5192340d78d
Lachlan Leone <starfluxgames@gmail.com>
2025-01-26 16:16:10 +1100

    Added Kangaroo Outfit

---
+++ Customs/PlayerCosmetics/KangarooOutfit.cs
@@ -0,0 +1,26 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using UnityEngine;
+
+namespace AussieClassics.Customs.PlayerCosmetics
+{
+    public class KangarooOutfit : CustomPlayerCosmetic
+    {
+        public override string UniqueNameID => "KangarooOutfit";
+        public override CosmeticType CosmeticType => CosmeticType.Outfit;
+        public override GameObject Visual => Mod.Bundle.LoadAsset<GameObject>("KangarooHead").AssignMaterialsByNames();
+        // public override bool BlockHats => true;
+        public override float HeadSize => 0.4f;
+
+        public override void OnRegister(PlayerCosmetic gameDataObject)
+        {
+            PlayerOutfitComponent playerOutfitComponent = gameDataObject.Visual.AddComponent<PlayerOutfitComponent>();
+            playerOutfitComponent.Renderers.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Head").GetComponent<SkinnedMeshRenderer>());
+            //playerOutfitComponent.Hats.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Head").GetComponent<SkinnedMeshRenderer>());
+            playerOutfitComponent.Renderers.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Tail").GetComponent<SkinnedMeshRenderer>());
+            
+        }
+    }
+}
